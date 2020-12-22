20ec71ec93520206b895164111e1552c0fe88728
Jeff Tian <jeff.tian@outlook.com>
2020-12-22 14:14:40 +0800

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,23 @@ import { extendedGrpcOptions } from 'grpc-health/dist/health/health-grpc-client.
 import { GrpcOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import * as LogRocket from 'logrocket';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Shopping Cart')
+    .setDescription('The shopping cart API description')
+    .setVersion('1.0')
+    .addTag('shopping-cart')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   app.connectMicroservice(
     extendedGrpcOptions(grpcClientOptions as GrpcOptions),
