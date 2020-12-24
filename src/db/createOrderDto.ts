c91c3ddf405eaf0ef83dac0050a7383e45e87e6e
Jeff Tian <jeff.tian@outlook.com>
2020-12-24 14:14:51 +0800

    fix: swagger ApiProperty

--- src/db/createOrderDto.ts
+++ src/db/createOrderDto.ts
@@ -1,10 +1,15 @@
 import { OrderStatus } from './entities/orders.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderDto {
+  @ApiProperty()
   cents: number;
 }
 
 export class UpdateOrderDto {
+  @ApiProperty()
   orderId: number;
+
+  @ApiProperty()
   newStatus: OrderStatus;
 }
