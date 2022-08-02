af2d11a36f4e2301daa7f94c996687567f20b98f
cnjeftia <jeff.tian@outlook.com>
2022-08-02 14:35:13 +0800

    fix: tests

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,19 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
 import { DNSHealthIndicator, HealthCheckService } from '@nestjs/terminus';
-import { HttpService } from '@nestjs/common';
 import { HealthCheckExecutor } from '@nestjs/terminus/dist/health-check/health-check-executor.service';
+import { HttpModule } from '@nestjs/common';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [],
+      imports: [HttpModule],
       controllers: [HealthController],
       providers: [
         { provide: 'AXIOS_INSTANCE_TOKEN', useValue: 'AXIOS_INSTANCE_TOKEN' },
-        HttpService,
         DNSHealthIndicator,
         HealthCheckExecutor,
         HealthCheckService,
