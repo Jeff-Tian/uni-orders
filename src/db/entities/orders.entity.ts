9041172526ed1ae7150abfd336b01a7832917081
cnjeftia <jeff.tian@outlook.com>
2022-08-02 10:49:39 +0800

    docs: add api properties for order type

--- src/db/entities/orders.entity.ts
+++ src/db/entities/orders.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 export enum OrderStatus {
   Created,
@@ -10,38 +11,48 @@ export enum OrderStatus {
 @Entity()
 export class Order {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column({ unique: true })
+  @ApiProperty()
   number: string;
 
   @Column({})
+  @ApiProperty()
   cents: number;
 
   @Column({})
+  @ApiProperty()
   randomDiscountCents: number;
 
   @Column()
+  @ApiProperty()
   status: OrderStatus;
 
   @Column()
+  @ApiProperty()
   created_at: Date;
 
   @Column({
     nullable: true,
   })
+  @ApiProperty()
   paid_at?: Date;
 
   @Column({
     nullable: true,
   })
+  @ApiProperty()
   cancelled_at?: Date;
 
   @Column({
     nullable: true,
   })
+  @ApiProperty()
   timeout_at?: Date;
 
   @Column()
+  @ApiProperty()
   remark: string;
 }
