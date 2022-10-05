290f0af104e20de587383eeb761d35c297599b83
Jeff Tian@Workstation <jie.tian@live.cn>
2022-10-05 10:03:23 +0800

    fix: use seconds for time

--- src/payments/payment.service.ts
+++ src/payments/payment.service.ts
@@ -1,49 +1,50 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
-import { Order } from '../db/entities/orders.entity';
-import { symbols } from '../constants';
-import { Repository } from 'typeorm';
-import { Payment } from '../db/entities/payments.entity';
-import { OrderStatus } from '../db/entities/orderStatus';
-import { HttpService } from '@nestjs/axios';
+import {Inject, Injectable, Logger} from '@nestjs/common';
+import {Order} from '../db/entities/orders.entity';
+import {symbols} from '../constants';
+import {Repository} from 'typeorm';
+import {Payment} from '../db/entities/payments.entity';
+import {OrderStatus} from '../db/entities/orderStatus';
+import {HttpService} from '@nestjs/axios';
 
 @Injectable()
 export class PaymentService {
-  private readonly logger = new Logger(PaymentService.name);
+    private readonly logger = new Logger(PaymentService.name);
 
-  constructor(
-    @Inject(symbols.PAYMENTS_REPOSITORY)
-    private paymentRepo: Repository<Payment>,
-    @Inject(symbols.ORDERS_REPOSITORY) private orderRepo: Repository<Order>,
-    private readonly httpService: HttpService,
-  ) {}
-
-  public async findAndUpdatePaymentsFor(order: Order) {
-    const payments = await this.findPaymentsFor(order);
-    this.logger.log(`订单 ${order.id} 的支付记录查询结果是： ${payments}`)
-
-    if (payments && payments.length > 0) {
-      order.paid_at = new Date(payments[0].pay_time * 1000);
-      order.status = OrderStatus.Paid;
-      await this.orderRepo.save(order);
-
-      payments.forEach((payment) => {
-        payment.order_id = order.id;
-        this.paymentRepo.save(payment);
-      });
+    constructor(
+        @Inject(symbols.PAYMENTS_REPOSITORY)
+        private paymentRepo: Repository<Payment>,
+        @Inject(symbols.ORDERS_REPOSITORY) private orderRepo: Repository<Order>,
+        private readonly httpService: HttpService,
+    ) {
     }
-  }
 
-  private async findPaymentsFor(order: Order): Promise<Payment[]> {
-    const url = `https://leg-godt.azurewebsites.net/api/wecom/Bill/hardmoney/${order.created_at.getTime()}?cents=${order.cents}`;
+    public async findAndUpdatePaymentsFor(order: Order) {
+        const payments = await this.findPaymentsFor(order);
+        this.logger.log(`订单 ${order.id} 的支付记录查询结果是： ${payments}`)
 
-    this.logger.log(`查询订单 ${order.id} 的 url 是 ${url}`);
+        if (payments && payments.length > 0) {
+            order.paid_at = new Date(payments[0].pay_time * 1000);
+            order.status = OrderStatus.Paid;
+            await this.orderRepo.save(order);
 
-    const res = await this.httpService
-      .get(
-        url,
-      )
-      .toPromise();
+            payments.forEach((payment) => {
+                payment.order_id = order.id;
+                this.paymentRepo.save(payment);
+            });
+        }
+    }
 
-    return res.data;
-  }
+    private async findPaymentsFor(order: Order): Promise<Payment[]> {
+        const url = `https://leg-godt.azurewebsites.net/api/wecom/Bill/hardmoney/${Math.floor(order.created_at.getTime() / 1000)}?cents=${order.cents}`;
+
+        this.logger.log(`查询订单 ${order.id} 的 url 是 ${url}`);
+
+        const res = await this.httpService
+            .get(
+                url,
+            )
+            .toPromise();
+
+        return res.data;
+    }
 }
