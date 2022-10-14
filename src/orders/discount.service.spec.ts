8ead4452f05669457b793f31d54f30ffa42c64e7
cnjeftia <jeff.tian@outlook.com>
2022-10-14 20:12:27 +0800

    feat: throws when no available discounts

--- src/orders/discount.service.spec.ts
+++ src/orders/discount.service.spec.ts
@@ -10,4 +10,13 @@ describe('discount service', () => {
     it('gets a discount less than 100', async () => {
         expect(await sut.getDiscount()).toBeLessThan(100);
     })
+
+    it('throws when there is no available discounts', async()=>{
+        const newDiscountService = new DiscountService();
+
+        const oneHundredArray = new Array(100).fill(0);
+        await Promise.all(oneHundredArray.map(newDiscountService.getDiscount.bind(newDiscountService)))
+
+        await expect(newDiscountService.getDiscount.bind(newDiscountService)).rejects.toThrowError("No Available Discount for Now!")
+    })
 })
