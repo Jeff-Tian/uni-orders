86db771e42433f32780a9e1f4112e23341265125
Jeff Tian <jeff.tian@outlook.com>
2022-10-18 19:39:46 +0800

    feat: reset when size is less than or equal to 0

--- src/discounts/db.discount.storage.ts
+++ src/discounts/db.discount.storage.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { symbols } from '../constants';
 import { Repository } from 'typeorm';
 import { Discount } from '../db/entities/discounts.entity';
+import { initialDiscounts } from './inmemory.discount.storage';
 
 @Injectable()
 export class DbDiscountsStorage implements IDiscountsStorage {
@@ -12,7 +13,13 @@ export class DbDiscountsStorage implements IDiscountsStorage {
   ) {}
 
   async getSize(): Promise<number> {
-    return await this.discountRepo.count();
+    const size = await this.discountRepo.count();
+
+    if (size <= 0) {
+      await this.reset();
+    }
+
+    return size;
   }
 
   async pop(): Promise<number | undefined> {
@@ -27,7 +34,9 @@ export class DbDiscountsStorage implements IDiscountsStorage {
     return res.value;
   }
 
-  reset(): Promise<void> {
-    return Promise.resolve();
+  async reset(): Promise<void> {
+    await Promise.all(
+      initialDiscounts.map((value) => this.discountRepo.save({ value })),
+    );
   }
 }
