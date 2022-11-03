3d2cba09db34079ac21491795c4d4f74d384c161
Jeff Tian <jeff.tian@outlook.com>
2022-11-03 10:29:57 +0800

    feat: still change back to asynchronosly update order

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -67,7 +67,7 @@ export class OrdersService implements IOrdersService {
       order.status !== OrderStatus.Paid
     ) {
       this.logger.log(`查询支付记录 ${JSON.stringify(order)}`);
-      this.paymentService.findAndUpdatePaymentsFor(order).then((res) => {
+      await this.paymentService.findAndUpdatePaymentsFor(order).then((res) => {
         this.logger.log(`支付记录异步更新结果： ${util.inspect(res)}`);
       });
     }
